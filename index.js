2e421bd8089b6b7c27a964cda1b6c7a085da384e
NoobZombieTR <38327391+NoobZombieTR@users.noreply.github.com>
2018-04-21 16:13:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
-function makeChannel(message){
-    var server = message.guild;
-    var name = message.author.username;
 
-    server.createChannel(name, "orbit-log");
-}
 
 bot.on("ready", async () => {
 	
